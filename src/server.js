ff36d7de4fc4f0a6ee4bad4ffb084c3dbcb89f4d
haydonclore <cac6708@g.rit.edu>
2014-10-31 11:25:33 -0400

    Updates to use express

--- src/server.js
+++ src/server.js
@@ -1,8 +1,31 @@
-//import the websocket library. There are many, but socket.io is one of the most common and feature rich
+var path = require('path');
+var express = require('express');
+var compression = require('compression');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var router = require('./router.js');
+
+
 var socketio = require('socket.io');
 
-//start a socketio server and grab the server as io
-var io = socketio.listen(31415);
+var app = express();
+app.use('/assets', express.static(path.resolve(__dirname+'../../client/')));
+app.use(compression());
+app.set('view engine', 'jade');
+app.set('views', __dirname+'../../client/template');
+app.use(favicon(__dirname+'../../client/img/favicon.png'));
+app.use(cookieParser());
+
+router(app);
+var port = process.env.PORT || process.env.NODE_PORT || 5000;
+var server = app.listen(port, function(err){
+	if(err)
+		throw err;
+	console.log('listening on port ' + port);
+});
+
+
+var io = socketio.listen(server);
 
 //object to hold all of our connected users
 var users = {};
@@ -15,7 +38,7 @@ var onJoined = function(socket) {
         users[socket.name] = socket.name;
 		
         socket.join('room1');
-		console.log(socket.name + ' joined');
+		//console.log(socket.name + ' joined');
 		
 		socket.emit('helloClient', { name: socket.name });
 	});
@@ -38,14 +61,14 @@ var onDisconnect = function(socket) {
 	});
 };
 
-console.log('starting up');
+console.log('starting up...');
 
 //tell the server what to do when new sockets connect
 //'connection' is a built-in event from socketio that fires any time a new connection occurs
 //The 'connection' event automatically sends the newly connected socket to the function 
 io.sockets.on("connection", function(socket) {
 
-    console.log('started');
+    console.log('connected to new client');
     
     //call the functions to attach handlers and send in the new socket connect
     onJoined(socket); //pass socket to onJoined to attach joined event
